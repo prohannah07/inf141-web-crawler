c0267e2962bc4fcc8a875d18392b7637c62bf99c
MantaRey <reymunme@uci.edu>
2020-02-04 04:08:23 -0800

    Uuuuuhhhhh fixed???

--- Project2StarterCode/crawler.py
+++ Project2StarterCode/crawler.py
@@ -81,7 +81,7 @@ class Crawler:
             soup = BeautifulSoup(url_data["content"], 'lxml-xml')
             for link in soup.find_all('a', href=True):
                 extracted_link = link.get('href')
-                if "ttp" not in extracted_link:
+                if "http" not in extracted_link:
                     # print(link.get('href'))
                     if url_data["is_redirected"] == True:
                         web_url = url_data["final_url"]
@@ -108,36 +108,10 @@ class Crawler:
         if parsed.scheme not in set(["http", "https"]):
             return False
         try:
-            # return ".ics.uci.edu" in parsed.hostname \
-            #        and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4"
-            #                         + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            #                         + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1"
-            #                         + "|thmx|mso|arff|rtf|jar|csv"
-            #                         + "|sql|htm|java|prefs|class|h|cc|cpp|svn"
-            #                         + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower()) \
-            #     and parsed.fragment == "" \
-            #     and not re.match("^.*?(/.+?/).*?\1.*$|^.*?/(.+?/)\2.*$", parsed.path.lower()) \
-            #     and not re.match("^.*calendar.*$", parsed.path.lower()) \
-            #     and not "replytocom" in parsed.path \
-            #     and not ("grape" in parsed.hostname and (re.match("^.*attachment.*$", parsed.path.lower()) or re.match("^.*timeline.*$", parsed.path.lower())))
-            #                  \
-            # and parsed.fragment == "" \
-            # and False if( "grape" in parsed.hostname and "attachment" in parsed.path ) else True \
-            # and not "replytocom" in parsed.path
-
-            if "ics.uci.edu" not in parsed.hostname or parsed.fragment != "" or \
-            re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" +
-                    "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" +
-                    "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" +
-                    "|thmx|mso|arff|rtf|jar|csv" +
-                    "|sql|htm|java|prefs|class|h|cc|cpp|svn|txt" +
-                    "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower()) or \
-            re.match ("grape" in parsed.hostname and (re.match("^.*attachment.*$", parsed.path.lower()) or re.match("^.*timeline.*$", parsed.path.lower()))):
+            if "ics.uci.edu" not in parsed.hostname or parsed.fragment != "" or re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" + "|thmx|mso|arff|rtf|jar|csv" + "|sql|htm|java|prefs|class|h|cc|cpp|svn|txt" + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower()) or ("grape" in parsed.hostname and (re.match("^.*attachment.*$", parsed.path) or re.match("^.*timeline.*$", parsed.path))):
                 return False
                 
-            elif re.match("^.*?(/.+?/).*?\1.*$|^.*?/(.+?/)\2.*$", parsed.path.lower()) or \
-            ("calendar" not in parsed.hostname and re.match("^.*calendar.*$", parsed.path.lower())) or \
-            re.match "replytocom" in parsed.path:
+            elif re.match("^.*?(/.+?/).*?\1.*$|^.*?/(.+?/)\2.*$", parsed.path.lower()) or ("calendar" not in parsed.hostname and re.match("^.*calendar.*$", parsed.path.lower())) or re.match("replytocom", parsed.path.lower()):
                 self.trap_links_file.write(url + "\n")
                 return False
 
--- Project2StarterCode/main.py
+++ Project2StarterCode/main.py
@@ -30,47 +30,6 @@ if __name__ == "__main__":
 
     # Instantiates a crawler object and starts crawling
 
-    # https://www.ics.uci.edu/~sjordan/research/device attachment.html
-    # https://grape.ics.uci.edu/wiki/asterix/raw-attachment/wiki/stats170ab-2018/postgresloading.sql
-    # parsed = urlparse("https://grape.ics.uci.edu/wiki/asterix/raw-attachment/wiki/stats170ab-2018/postgresloading.sql")
-    # print(parsed)
-    # if ".ics.uci.edu" in parsed.hostname :
-    #     print("Grape in Hostname")
-    # if re.match("^.*attachment.*$", parsed.path.lower()):
-    #     print("Also Attachment")
-    # parsed = urlparse("https://grape.ics.uci.edu/wiki/asterix/timeline?from=2018-04-02T15%3A21%3A31-07%3A00&precision=second")
-    # print(parsed)
-    # if ".ics.uci.edu" in parsed.hostname :
-    #     print("Grape in Hostname")
-    # if re.match("^.*timeline.*$", parsed.path.lower()):
-    #     print("Also Timeline")
-    # parsed = urlparse("https://www.ics.uci.edu/~minhaenl/MH/MHP2G/MHP2G%20v1.2.htm")
-    # print(parsed)
-    # parsed = urlparse("https://www.ics.uci.edu/~emilyo/teaching/info122w2014/samplecode/lecture8/AbstractFactory/src/NYPizzaStore.java")
-    # print(parsed)
-    # parsed = urlparse("https://www.ics.uci.edu/~emilyo/teaching/info122w2014/samplecode/lecture8/FactoryMethod/.settings/org.eclipse.jdt.core.prefs")
-    # print(parsed)
-    # parsed = urlparse("https://ics.uci.edu/~emilyo/teaching/info122w2014/samplecode/lecture8/FactoryMethod/bin/PizzaStore.class")
-    # print(parsed)
-    # print("True" if "ics.uci.edu" not in parsed.hostname else "False")
-    # parsed = urlparse("http://flamingo.ics.uci.edu/releases/3.0/src/stringmap/split_l.h")
-    # print(parsed)
-    # parsed = urlparse("http://flamingo.ics.uci.edu/releases/account/3.0/src/topk/example.pdf")
-    # print(parsed)
-    # print( "True" if re.match(".*\.cc$", parsed.path.lower()) else "False" )
-    # parsed = urlparse("http://calendar.ics.uci.edu")
-    # print(parsed)
-    # print( "True" if re.match("^.*calendar.*$", parsed.path.lower()) else "False" )
-    # parsed = urlparse("http://town.com/calendar/cows")
-    # print(parsed)
-    # print( "True" if re.match("^.*calendar.*$", parsed.path.lower()) else "False" )
-    # parsed = urlparse("http://flamingo.ics.uci.edu/releases/4.1/src/filtertree/data/.svn/entries")
-    # print(parsed)
-
-    # file_path = 'stats.txt'
-
-    # fp = open(file_path, 'w')
-
     analytics_path1 = 'analytics_part1.txt'
     analytics_path2 = 'analytics_part2.txt'
     analytics_path3 = 'analytics_part3.txt'
@@ -81,8 +40,6 @@ if __name__ == "__main__":
 
     crawler = Crawler(frontier, corpus, ap2, ap3)
     crawler.start_crawling()
-
-    # fp.close()
 
     analytics.display_analytics(ap1)
 
